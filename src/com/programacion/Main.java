c4bbbaf0a52e903e10f4e031315062aab78bc191
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-12-14 17:18:59 +0100

    Condiciionales

---
+++ src/com/programacion/Main.java
@@ -0,0 +1,14 @@
+package com.programacion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Short obx = new Short();
+        obx.amosarCalculos();
+
+
+
+
+
+    }
+}
---
+++ src/com/programacion/Short.java
@@ -0,0 +1,25 @@
+package com.programacion;
+
+import javax.swing.*;
+
+public class Short {
+
+    public void amosarCalculos(){
+
+        short num1, num2;
+
+        num1 = (short) Integer.parseInt(JOptionPane.showInputDialog(" numero 1 "));
+        num2 = (short) Integer.parseInt(JOptionPane.showInputDialog(" numero 2 "));
+
+        if ( num1 >= num2){
+            JOptionPane.showMessageDialog(null, " a resta entre " + num1 + " e " + num2 + " e igual a " + (num1 - num2) + "\n" + " ademas, la suma es igual a " + (num1 + num2));
+
+        }
+        else
+            JOptionPane.showMessageDialog(null," a suma entre " + num1 + " e " + num2 + " e igual a " + (num1 + num2) );
+
+
+
+    }
+
+}
